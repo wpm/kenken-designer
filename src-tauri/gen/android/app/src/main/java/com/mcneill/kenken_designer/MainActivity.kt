2c35c0e2af2c91af12e2d8635fa66ae8ac8a0047
W.P. McNeill <billmcn@gmail.com>
2026-05-02 19:41:58 -0500

    feat: Generic Tauri/Leptos Application

---
+++ src-tauri/gen/android/app/src/main/java/com/mcneill/kenken_designer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mcneill.kenken_designer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
